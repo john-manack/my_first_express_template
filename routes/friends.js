5edc45478f0bdc59ae552284e9097aa6b9c1fe58
John Manack <jmnck28@gmail.com>
2021-01-27 16:06:25 -0500

    adds new html templates

--- routes/friends.js
+++ routes/friends.js
@@ -5,28 +5,29 @@ const express = require("express"),
 
 const friendsArray = require('../db');
 
-// When you're in a route file, the ROOT points to itself. That's why we use a '/' below instead of '/friends'.
 router.get('/', (req, res) => {
-    let htmlData = `<ul>`;
-    friendsArray.map((friend) => {
-        htmlData += `<li><a href="./friends/${friend.handle}">${friend.name}</a></li>`
-    })
-    htmlData += `</ul> `;
-    res.send(htmlData);
+    res.render('friends', {
+        locals: {
+            friends: friendsArray
+        }
+    });
 });
 
-// The colon below in the first argument of the function denotes a parameter, and makes 'handle' available as a variable to call with 'req.params.handle'.
 router.get('/:handle', (req, res) => {
-    const { handle } = req.params;
+    const { handle } = req.params; // this is called DESTRUCTURING
     const friend = friendsArray.find((friend) => {
         if (friend.handle === handle) {
             return friend;
         }
     });
     if (friend) {
-        res.send(`<h1>Hi ${friend.name}!</h1>`)
+        res.render('friend', {
+            locals: {
+                friend: friend // the 2nd friend (to the right of the colon), equals the variable we defined on line 14
+            }
+        });
     } else {
-        res.send(`No friend with the handle, ${handle}, was found :(`)
+        res.send(`No friends with tag ${handle}`)
     }
 })
 
